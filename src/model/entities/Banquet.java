53864f5de1b97a1dbd565bf29fda45d4f73bef5a
FrankYang0610 <yxk0610@icloud.com>
2024-11-13 00:03:23 +0800

    Refactor

--- src/model/entities/Banquet.java
+++ src/model/entities/Banquet.java
@@ -1,17 +1,17 @@
-package model;
+package model.entities;
 import java.sql.Date;
 
 public class Banquet {
-    private int BIN;
+    private int BIN; // should we use long here?
     private String Name;
-    private Date DateTime;
+    private Date DateTime; // should we use date here?
     private String Address;
     private String Location;
     private String ContactStaffName;
     private boolean Available;
-    private int quota;
+    private int Quota;
 
-    public Banquet(int BIN, String Name, Date DateTime, String Address, String Location, String ContactStaffName, boolean Available, int quota) {
+    public Banquet(int BIN, String Name, Date DateTime, String Address, String Location, String ContactStaffName, boolean Available, int Quota) {
         this.BIN = BIN;
         this.Name = Name;
         this.DateTime = DateTime;
@@ -19,6 +19,6 @@ public class Banquet {
         this.Location = Location;
         this.ContactStaffName = ContactStaffName;
         this.Available = Available;
-        this.quota = quota;
+        this.Quota = Quota;
     }
 }
--- src/model/managers/BanquetsManager.java
+++ src/model/managers/BanquetsManager.java
@@ -1,8 +1,9 @@
-package model;
+package model.managers;
+import model.ModelException;
+import model.entities.Banquet;
+
 import java.sql.*;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 /**
  * <h2> Banquets Class </h2>
@@ -11,109 +12,51 @@ import java.util.NoSuchElementException;
  *
  * Implements basic operations {@code addBanquet}, {@code getAllBanquets}, {@code getBanquet}, {@code updateBanquet}, {@code deleteBanquet}
 */
-public class BanquetsManager extends EntityManager {
-    public BanquetsManager() {
-        // Initialize database connection in constructor
+public class BanquetsManager {
+    private final Connection con;
+
+    public BanquetsManager(Connection con) throws ModelException {
+        this.con = con;
         initializeBanquets();
     }
 
-    private void initializeBanquets() {
-        String createTableSQL = """
+    private void initializeBanquets() throws ModelException {
+        String stmt = """
             CREATE TABLE IF NOT EXISTS BANQUETS (
                 BIN INTEGER PRIMARY KEY AUTO_INCREMENT,
                 Name VARCHAR(255) NOT NULL,
                 DateTime DATETIME NOT NULL,
-                Location VARCHAR(255) NOT NULL,
                 Address VARCHAR(255) NOT NULL,
-                Type VARCHAR(255) NOT NULL,
+                Location VARCHAR(255) NOT NULL,
                 ContactStaffName VARCHAR(255) NOT NULL,
                 Available BOOLEAN NOT NULL,
-                Quota INT NOT NULL,
+                Quota INTEGER NOT NULL
             )
         """;
-        dbConnection.executeUpdate(createTableSQL);
+
+        try (PreparedStatement pstmt = con.prepareStatement(stmt)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new ModelException("Cannot initialize the database table.");
+        }
     }
 
     /** Retrieves all banquet records from the database.
      * <p>This method executes a SQL query to select all entries from the {@code banquets}
      * table and constructs a list of {@code Banquet} objects representing each record.</p>
-      * @return A {@code List} containing all {@code Banquet} objects from the database.
-     * @throws RuntimeException if a database access error occurs or the SQL query fails.
+     * @return A {@code List} containing all {@code Banquet} objects from the database.
+     * @throws ModelException if any errors encountered.
      */
-    public List<Banquet> getAllBanquets() {
+    public List<Banquet> getAllBanquets() throws ModelException {
         String selectAllSQL = "SELECT * FROM BANQUETS";
-        List<Banquet> banquets = new ArrayList<>();
-        try(PreparedStatement pstmt = dbConnection.prepareStatement(selectAllSQL)) {
-            ResultSet resultSet = pstmt.executeQuery();
-            while (resultSet.next()) {
-                banquets.add(new Banquet(resultSet.getInt(1), resultSet.getString(2),
-                        resultSet.getDate(3), resultSet.getString(4),
-                        resultSet.getString(5), resultSet.getString(6),
-                        resultSet.getBoolean(7), resultSet.getInt(8)));
-            }
-            return banquets;
-        } catch (SQLException e) {
-            throw new RuntimeException("Database error: " + e.getMessage(), e);
-        }
-    }
-
-    /**
-     * Retrieves a {@code Banquet} object from the database based on the provided banquet ID.
-     *
-     * @param BIN the unique identifier of the banquet to retrieve.
-     * @return the {@code Banquet} object corresponding to the provided ID.
-     * @throws NoSuchElementException if no banquet with the specified ID exists.
-     * @throws RuntimeException       if a database access error occurs.
-     */
-    public Banquet getBanquet(int BIN) {
-        String selectSQL = "SELECT * FROM BANQUETS WHERE BIN = " + BIN;
-
-        try (PreparedStatement pstmt = dbConnection.prepareStatement(selectSQL)){
-            ResultSet resultSet = pstmt.executeQuery();
-
-            if (resultSet.next()) {
-                return new Banquet(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getDate(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getString(6),
-                        resultSet.getBoolean(7),
-                        resultSet.getInt(8));
-            } else {
-                throw new NoSuchElementException("Banquet with ID " + BIN + " not found.");
-            }
-        } catch(SQLException e) {
-            throw new RuntimeException("Database error: " + e.getMessage(), e);
-        }
-    }
-
-    /**
-     * Retrieves a list of {@code Banquet} objects from the database based on a specified column and value.
-     * <p>
-     * This method executes a SQL query to select all entries from the {@code banquets}
-     * table where the specified column matches the provided value.
-     *
-     * @param attribute the column name to filter the banquets by. Must be one of the allowed columns.
-     * @param newValue  the value to match in the specified column.
-     * @return a {@code List} containing {@code Banquet} objects that match the criteria.
-     * @throws RuntimeException         if a database access error occurs.
-     */
-
-    public List<Banquet> getBanquet(String attribute, String newValue){
-        String selectSQL = "SELECT * FROM BANQUETS WHERE " + attribute + " = ?";
 
         List<Banquet> banquets = new ArrayList<>();
 
-        try (PreparedStatement pstmt = dbConnection.prepareStatement(selectSQL)) {
-            pstmt.setString(1, newValue);
-
+        try (PreparedStatement pstmt = con.prepareStatement(selectAllSQL)) {
             ResultSet resultSet = pstmt.executeQuery();
 
             while (resultSet.next()) {
-                banquets.add(new Banquet(
-                        resultSet.getInt(1),
+                banquets.add(new Banquet(resultSet.getInt(1),
                         resultSet.getString(2),
                         resultSet.getDate(3),
                         resultSet.getString(4),
@@ -125,7 +68,77 @@ public class BanquetsManager extends EntityManager {
 
             return banquets;
         } catch (SQLException e) {
-            throw new RuntimeException("Database error: " + e.getMessage(), e);
+            throw new ModelException("Database error: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Retrieves a {@code Banquet} object from the database based on the provided banquet ID.
+     *
+     * @param BIN the unique identifier of the banquet to retrieve.
+     * @return the {@code Banquet} object corresponding to the provided ID.
+     * @throws ModelException if any errors encountered.
+     */
+    public Banquet getBanquet(int BIN) throws ModelException {
+        String selectSQL = "SELECT * FROM BANQUETS WHERE BIN = " + BIN;
+
+        try (PreparedStatement pstmt = con.prepareStatement(selectSQL)) {
+            ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return new Banquet(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getString(6),
+                        resultSet.getBoolean(7),
+                        resultSet.getInt(8));
+            } else {
+                throw new ModelException("Banquet with ID " + BIN + " not found.");
+            }
+        } catch (SQLException e) {
+            throw new ModelException("Database error: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Retrieves a list of {@code Banquet} objects from the database based on a specified column and value.
+     * <p>
+     * This method executes a SQL query to select all entries from the {@code banquets}
+     * table where the specified column matches the provided value.
+     *
+     * <h4>Usage examples</h4>
+     * <blockquote><pre>
+     *     List<Banquet> testBanquets = getBanquet("Name", "TestBanquet");
+     * </pre></blockquote>
+     *
+     * @param attribute the column name to filter the banquets by. Must be one of the allowed columns.
+     * @param value the value to match in the specified column.
+     * @return a {@code List} containing {@code Banquet} objects that match the criteria.
+     * @throws ModelException if any errors encountered.
+     */
+    public List<Banquet> getBanquet(String attribute, String value) throws ModelException {
+        String stmt = "SELECT * FROM BANQUETS WHERE " + attribute + " = " + value;
+
+        List<Banquet> banquets = new ArrayList<>();
+
+        try (PreparedStatement pstmt = con.prepareStatement(stmt)) {
+            ResultSet resultSet = pstmt.executeQuery();
+            while (resultSet.next()) {
+                banquets.add(new Banquet(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getString(6),
+                        resultSet.getBoolean(7),
+                        resultSet.getInt(8)));
+            }
+            return banquets;
+        } catch (SQLException e) {
+            throw new ModelException("Database error: " + e.getMessage());
         }
     }
 
@@ -138,23 +151,17 @@ public class BanquetsManager extends EntityManager {
      * @param BIN the unique identifier of the banquet to update.
      * @param attribute    the column name to update. Must be one of the allowed columns.
      * @param newValue     the new value to set for the specified column.
-     * @throws NoSuchElementException   if no banquet with the specified ID exists.
-     * @throws RuntimeException         if a database access error occurs.
+     * @throws ModelException if any errors encountered.
      */
+    public void updateBanquet(int BIN, String attribute, String newValue) throws ModelException { // This method should be improved later.
+        String stmt = "UPDATE BANQUETS SET " + attribute + " = " + newValue + " WHERE BIN = " + BIN; // should this be adopted?
 
-    public void updateBanquet(int BIN, String attribute, String newValue) {
-        String updateSQL = "UPDATE BANQUETS SET " + attribute + " = ? WHERE BIN = ?";
-
-        try (PreparedStatement pstmt = dbConnection.prepareStatement(updateSQL)) {
-            pstmt.setString(1, newValue);
-            pstmt.setInt(2, BIN);
-
-            int affectedRowsCnt = pstmt.executeUpdate();
-            if (affectedRowsCnt == 0) {
+        try (PreparedStatement pstmt = con.prepareStatement(stmt)) {
+            if (/* affectedRowCnt = */ pstmt.executeUpdate() == 0) {
                 throw new NoSuchElementException("Banquet with BIN " + BIN + " not found.");
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Database error: " + e.getMessage(), e);
+            throw new ModelException("Database error: " + e.getMessage());
         }
     }
 
@@ -162,20 +169,19 @@ public class BanquetsManager extends EntityManager {
      * Deletes a {@code Banquet} record from the database based on the provided banquet ID.
      *
      * @param BIN the unique identifier of the banquet to delete.
-     * @throws NoSuchElementException if no banquet with the specified ID exists.
-     * @throws RuntimeException       if a database access error occurs.
+     * @throws ModelException if any errors encountered.
      */
+    public void deleteBanquet(int BIN) throws ModelException {
+        String stmt = "DELETE FROM BANQUETS WHERE BIN = " + BIN;
 
-    public void deleteBanquet(int BIN) {
-        String deleteSQL = "DELETE FROM BANQUETS WHERE BIN = " + BIN;
-
-        try (PreparedStatement pstmt = dbConnection.prepareStatement(deleteSQL)) {
+        try (PreparedStatement pstmt = con.prepareStatement(stmt)) {
             int affectedRows = pstmt.executeUpdate();
+
             if (affectedRows == 0) {
-                throw new NoSuchElementException("Banquet with BIN " + BIN + " not found.");
+                throw new ModelException("Banquet with BIN " + BIN + " not found.");
             }
-        } catch(SQLException e){
-            throw new RuntimeException("Database error: " + e.getMessage(), e);
+        } catch (SQLException e) {
+            throw new ModelException("Database error: " + e.getMessage());
         }
     }
 }
